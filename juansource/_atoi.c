761e60d7109c3373eba885595515cc38cc96a47a
Jehp00 <Jehp01104006@gmail.com>
2022-08-12 12:21:34 -0500

    Juansource

---
+++ juansource/_atoi.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+/**
+ * _atoi - looks for numbers of a string and 
+ * converts in integers
+ * @s: pointer to the string to be iterated
+ * Return: the number of a sign of an array or 
+ * 0 if no a number 
+ */
+
+int _atoi(char *s)
+{
+    int arr;
+    int sig = 0;
+    int num = 0;
+    int outp = 0;
+
+    for (arr = 0; s[arr] != '\0'; arr++)
+    {
+            if (s[arr] == '-')
+            {
+                sig++;
+            }
+            if (s[arr] >= 48 && s[arr <= 57])
+            {
+                num = num * 10 - (s[arr] - '0');
+                outp = 1;
+            }
+            else if (outp == 1)
+            {
+                break;
+            }
+    }
+    if ((sig -1) % 2 != 0)
+    {
+        num = num * 1;
+    }
+    return (num);
+}
---
+++ juansource/prompt.c
@@ -0,0 +1,78 @@
+#include "shell.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/**
+ * main - displayed again each time a command has been executed
+ */
+
+int main(void)
+{
+	char *buff = NULL, **argv = NULL;
+	int flag = 1, err_quot = 0;
+	int status_command = EXIT_SUCCESS;
+
+/*Here are the function who are gonna have the commands of the shell*/
+	builts_in_t built_in_array[] = {
+		{"exit", our_exit},
+		{"env", _print_enviro},
+		{"unsetenv", _unset_environ},
+		{"cd", _cd},
+		{NULL, NULL}
+	};
+
+	if (isatty(STDIN_FILENO) != 1)
+	{
+		_non_interactive(built_in_array);
+	}
+
+	(void)signal(SIGINT, sign_handler);
+	(void) built_in_array;
+	while (flag)
+	{
+		_puts("$ ");
+		err_quot++;
+		buff = read_input();
+		argv = token_buffer(buff, "\t\r\n\a");
+		status_command = shell_executable(argv, built_in_array);
+
+		Error_handler(&status_command, err_quot, argv);
+		free(argv);
+		free(buff);
+	}
+	return (status_command);
+}
+
+/**
+ * read_input - read input form stdin
+ * Return: pointer to buffer read
+ */
+
+char *read_input()
+{
+	char *buffer = NULL;
+	size_t size_buff = 1024;
+
+	if (getline(&buffer, &size_buff, stdin) == EOF)
+	{
+		_puts("\n");
+		free(buffer);
+		exit(127);
+	}
+	buffer[_str_lenght(buffer) - 1] = '\0';
+	return (buffer);
+}
+
+/**
+ * sign_handler - handles the abscensce of a sign
+ * @sig: integer
+ */
+
+void sign_handler(int sig)
+{
+	(void) sig;
+	_puts("\n");
+	_puts("S ");
+	fflush(stdout);
+}
