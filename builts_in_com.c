442a731e09c605ea65ab2a9df2cc41f7ab93a16b
alejandrodgz <alejo@PC-Arturo>
2022-08-16 16:21:20 -0500

    task 5

--- builts_in_com.c
+++ builts_in_com.c
@@ -8,7 +8,7 @@
 
 int our_exit(char **argv)
 {
-	int status = 2, a;
+	int status = 0, a;
 
 	if (argv[1] == NULL)
 	{
@@ -27,7 +27,7 @@ int our_exit(char **argv)
 	status = _atoi(argv[1]);
 	free(argv[0]);
 	free(argv);
-	exit(0);
+	exit(status);
 }
 
 /**
